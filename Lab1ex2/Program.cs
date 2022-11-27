5bbe23d8ae2aa074d05b94944710f8d54f7869b0
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 15:54:58 +0200

    Add project files.

---
+++ Lab1ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va calcula media aritmetica a trei numere citite de la
+               tastatura */
+
+            Console.WriteLine("Introduceti primul numar ");
+            int numar1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar ");
+            int numar2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar ");
+            int numar3 = int.Parse(Console.ReadLine());
+
+            int mediaAritmetica = (numar1 + numar2 + numar3) / 3;
+            Console.WriteLine("Media aritmetica este: " + mediaAritmetica);
+            
+        }
+    }
+}
